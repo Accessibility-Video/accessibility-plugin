e118d636e2fc00160767bb307e80bee2d1977f0f
Quincy van Bommel <quincy@weave.nl>
2021-02-25 09:52:20 +0100

    build: transformed injection scripts into iife format

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,10 @@
 import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
+import { createFilter } from '@rollup/pluginutils';
 import { emptyDir } from 'fs-extra';
-import { chromeExtension, simpleReloader } from 'rollup-plugin-chrome-extension';
+import { rollup } from 'rollup';
+import { chromeExtension } from 'rollup-plugin-chrome-extension';
 import copy from 'rollup-plugin-copy';
 import { terser } from 'rollup-plugin-terser';
 
@@ -40,23 +42,59 @@ export default commandLineArgs => {
         config.output.sourcemap = 'inline';
     }
 
-    config.plugins.push(
+    const accessibleResources = createFilter([
+        'src/video-accessibility.ts',
+    ]);
+
+    const plugins = [
         typescript({ tsconfig: tsconfig }),
         nodeResolve(),
-        commonjs(),
-    );
-
-    if (commandLineArgs.environment === 'development') {
-        config.plugins.push(simpleReloader());
-    }
+    ];
 
     if (commandLineArgs.environment === 'production') {
-        config.plugins.push(terser({
+        plugins.push(terser({
             format: {
                 comments: false,
             },
         }));
     }
+
+    config.plugins.push(
+        {
+            name: 'accessible-resources',
+            resolveId(source, importer) {
+                if (!importer && accessibleResources(source)) {
+                    // this signals that rollup should not ask other
+                    // plugins or check the file system to find this id.
+                    return source;
+                }
+                return null;
+            },
+            async load(id) {
+                if (!accessibleResources(id)) return null;
+
+                const bundle = await rollup({
+                    input: id,
+                    plugins: plugins,
+                });
+
+                bundle.watchFiles.forEach((file) => {
+                    this.addWatchFile(file);
+                });
+
+                // Change output format so we can inject this in websites
+                const generator = await bundle.generate({
+                    format: 'iife',
+                    preserveModules: false,
+                });
+
+                return generator.output[0].code;
+            },
+
+        },
+        ...plugins,
+        commonjs(),
+    );
 
     return config;
 }
