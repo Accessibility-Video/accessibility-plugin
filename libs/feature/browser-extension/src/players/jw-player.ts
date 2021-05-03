8e8ce72e7a9da3ec5d0f0d19cb8ee200fc06469d
Quincy van Bommel <quincy@weave.nl>
2021-05-03 15:52:28 +0200

    feat: implemented JWPlayer with CC feature

--- libs/feature/browser-extension/src/players/jw-player.ts
+++ libs/feature/browser-extension/src/players/jw-player.ts
@@ -1,11 +1,27 @@
 import { A11y } from '@scribit/shared/types';
 import { FrameworkPlayer } from './framework-player';
 
-export class JWPlayer extends FrameworkPlayer<any> implements A11y.Toggle {
+export class JWPlayer extends FrameworkPlayer<jwplayer.JWPlayer> implements A11y.Toggle {
     /**
      * @inheritDoc
      */
-    protected getScanResults(): any[] {
-        return [];
+    protected getScanResults(element: Element): jwplayer.JWPlayer[] {
+        return Array.from(element.querySelectorAll('.jwplayer'))
+            .map(container => jwplayer(container))
+            .filter(player => player.getState());
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected toggleClosedCaptioning(enabled: boolean): void {
+        playerLoop: for (let player of this.players) {
+            for (let listItem of player.getCaptionsList()) {
+                if ((enabled && listItem.id !== 'off') || (!enabled && listItem.id === 'off')) {
+                    player.setCurrentCaptions(listItem.id);
+                    continue playerLoop;
+                }
+            }
+        }
     }
 }
