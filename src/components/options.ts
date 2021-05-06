8c991e786c81b3e78a420acc7e9aa0757633a58e
Quincy van Bommel <quincy@weave.nl>
2021-05-06 16:35:58 +0200

    chore: updated dependencies
    Replaced lit-element to new lit dependency.
    Also bumped version of other dependencies.

--- src/components/options.ts
+++ src/components/options.ts
@@ -1,6 +1,7 @@
 import { UserPreferences } from '@scribit/feature/browser-extension';
 import { A11y, Form } from '@scribit/shared/types';
-import { customElement, html, TemplateResult } from 'lit-element';
+import { html, TemplateResult } from 'lit';
+import { customElement } from 'lit/decorators.js'
 import { camelCase } from 'lodash'
 import { BaseComponent } from './base-component';
 import { Storage, t } from '../helpers';
