baf003e18b54b3e723e7f7b258285c1b0a5cd4be
Peter-Jan Karens <peterjan@weave.nl>
2021-05-28 18:57:03 +0200

    fix: firefox iife build

---
+++ firefoxfy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+dist=dist
+manifest=$dist/manifest.json
+
+if [ ! -e "$manifest" ]; then
+    echo "Could not target 'manifest.json'"
+    exit 1;
+fi
+##
+# Replaces the content scripts for the bundled ones.
+##
+npx rollup --config rollup.firefox.config.js --environment production
+sed -iE 's/assets\/foreground\(-[a-z]\{1,\}\)-[a-z0-9]\{1,\}.js/foreground\1.js/g' $manifest
