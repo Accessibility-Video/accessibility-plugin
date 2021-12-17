2ce777f54b21ed3ca2d372c45af4cd5980373ec8
William Rijksen <william.rijksen@thefuturepartners.nl>
2021-12-17 08:37:19 +0100

    fix: later loaded captions

--- libs/feature/browser-extension/src/players/jw-player.ts
+++ libs/feature/browser-extension/src/players/jw-player.ts
@@ -15,13 +15,34 @@ export class JWPlayer extends FrameworkPlayer<jwplayer.JWPlayer> implements A11y
      * @inheritDoc
      */
     protected toggleClosedCaptioning(enabled: boolean): void {
-        playerLoop: for (let player of this.players) {
-            for (let listItem of player.getCaptionsList()) {
-                if ((enabled && listItem.id !== 'off') || (!enabled && listItem.id === 'off')) {
-                    player.setCurrentCaptions(listItem.id);
-                    continue playerLoop;
+        for (let player of this.players) {
+            if (this.toggleCaptionsTrack(enabled, player) !== undefined) {
+                return;
+            }
+
+            // If none of the player tracks is used for captions, we add a
+            // listener which will listen until a captions text track is added.
+            const handleCaptionsListEvent = () => {
+                if (this.toggleCaptionsTrack(enabled, player) !== undefined) {
+                    player.off('captionsList', handleCaptionsListEvent);
                 }
+            };
+            player.on('captionsList', handleCaptionsListEvent);
+        }
+    }
+
+    private toggleCaptionsTrack(enabled: boolean, player: jwplayer.JWPlayer): boolean | undefined {
+        const captionsList = player.getCaptionsList();
+        if (!captionsList.length) {
+            return undefined;
+        }
+
+        for (let listItem of captionsList) {
+            if ((enabled && listItem.id !== 'off') || (!enabled && listItem.id === 'off')) {
+                player.setCurrentCaptions(listItem.id);
             }
         }
+
+        return enabled;
     }
 }
